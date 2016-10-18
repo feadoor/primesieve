da4148b143d33898dcd6aaf282b42c219e68af79
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2016-10-18 16:28:54 +0100

    Add implementation of Euler phi function

--- src/sieve/primefuncs.rs
+++ src/sieve/primefuncs.rs
@@ -18,7 +18,7 @@ impl Sieve {
         true
     }
 
-    /// Returns whether or not `n` is a prime number, or `None` if `n` is larger than the square
+    /// Returns whether or not `n` is a prime number, or `Err(())` if `n` is larger than the square
     /// of the largest prime held in the sieve.
     ///
     /// Uses a simple lookup if `n` is not greater than the largest number known about by the
@@ -29,38 +29,40 @@ impl Sieve {
     /// ```
     /// let sieve = primesieve::Sieve::to_limit(100);
     ///
-    /// assert_eq!(sieve.is_prime(0), Some(false));
-    /// assert_eq!(sieve.is_prime(1), Some(false));
-    /// assert_eq!(sieve.is_prime(2), Some(true));
-    /// assert_eq!(sieve.is_prime(3), Some(true));
-    /// assert_eq!(sieve.is_prime(4), Some(false));
-    /// assert_eq!(sieve.is_prime(5), Some(true));
+    /// assert_eq!(sieve.is_prime(0), Ok(false));
+    /// assert_eq!(sieve.is_prime(1), Ok(false));
+    /// assert_eq!(sieve.is_prime(2), Ok(true));
+    /// assert_eq!(sieve.is_prime(3), Ok(true));
+    /// assert_eq!(sieve.is_prime(4), Ok(false));
+    /// assert_eq!(sieve.is_prime(5), Ok(true));
     ///
-    /// assert_eq!(sieve.is_prime(491), Some(true));
-    /// assert_eq!(sieve.is_prime(493), Some(false));
-    /// assert_eq!(sieve.is_prime(495), Some(false));
-    /// assert_eq!(sieve.is_prime(497), Some(false));
-    /// assert_eq!(sieve.is_prime(499), Some(true));
+    /// assert_eq!(sieve.is_prime(491), Ok(true));
+    /// assert_eq!(sieve.is_prime(493), Ok(false));
+    /// assert_eq!(sieve.is_prime(495), Ok(false));
+    /// assert_eq!(sieve.is_prime(497), Ok(false));
+    /// assert_eq!(sieve.is_prime(499), Ok(true));
     ///
-    /// assert_eq!(sieve.is_prime(1000001), None);
+    /// assert_eq!(sieve.is_prime(1000001), Err(()));
     /// ```
-    pub fn is_prime(&self, n: u64) -> Option<bool> {
+    pub fn is_prime(&self, n: u64) -> Result<bool, ()> {
         match n {
-            2 | 3 | 5 => Some(true),
+            2 | 3 | 5 => Ok(true),
             _ => {
                 if n < self.limit() {
-                    Some(segment::get(&self.primes, n))
+                    Ok(segment::get(&self.primes, n))
                 } else if n <= self.limit().saturating_mul(self.limit()) {
-                    Some(Sieve::trial_division(&self, n))
+                    Ok(Sieve::trial_division(&self, n))
                 } else {
-                    None
+                    Err(())
                 }
             }
         }
     }
 
-    /// Factorises `n` into (prime, exponent) pairs. Returns Err(remainder, partial factorisation)
-    /// if `n` cannot be fully factorised without sieving for more primes.
+    /// Factorises `n` into (prime, exponent) pairs.
+    ///
+    /// Returns Err(remainder, partial factorisation) if `n` cannot be fully factorised without
+    /// sieving for more primes.
     ///
     /// If `x` is the largest number known about by the sieve, then any integer having at most one
     /// prime factor larger than `x` can be factorised. In particular, any number not greater than
@@ -121,5 +123,39 @@ impl Sieve {
         }
 
         Ok(factors)
+    }
+
+    /// Calculates the value of Euler's totient function `ϕ` at `n`.
+    ///
+    /// Uses the formula based on the factorisation of `n`, that is `ϕ(n)` is equal to `n` times
+    /// the product of `1 - 1/p`, where `p` ranges over the distinct prime factors of `n`.
+    ///
+    /// Returns `Err(())` if `n` cannot be factorised without first sieving for more primes.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let sieve = primesieve::Sieve::to_limit(100);
+    ///
+    /// assert_eq!(sieve.euler_phi(2), Ok(1));
+    /// assert_eq!(sieve.euler_phi(4), Ok(2));
+    /// assert_eq!(sieve.euler_phi(1 << 63), Ok(1 << 62));
+    ///
+    /// assert_eq!(sieve.euler_phi(2 * 3), Ok(2));
+    /// assert_eq!(sieve.euler_phi(89 * 97), Ok(88 * 96));
+    /// assert_eq!(sieve.euler_phi(8 * 9 * 5), Ok(4 * 6 * 4));
+    ///
+    /// assert_eq!(sieve.euler_phi(2 * 3 * 5 * 991), Ok(2 * 4 * 990));
+    /// assert_eq!(sieve.euler_phi(2 * 3 * 5 * 991 * 991), Err(()));
+    /// ```
+    pub fn euler_phi(&self, mut n: u64) -> Result<u64, ()> {
+        if let Ok(factors) = self.factorise(n) {
+            for (p, _) in factors {
+                n = (n / p) * (p - 1);
+            }
+            Ok(n)
+        } else {
+            Err(())
+        }
     }
 }
